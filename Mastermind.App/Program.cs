36e71ce2360628c69b865a72a11bcd865bdebfac
Brian <BrianKlinect@gmail.com>
2020-04-23 17:15:00 -0400

    Add project files.

---
+++ Mastermind.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mastermind.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
